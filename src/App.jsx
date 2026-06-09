6c6878ed043389721e5b17da14df3da62c305ace
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-06-09 14:10:44 +0530

    Handle Mobile Complexity in SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from './components/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 import ChatBox from './components/ChatBox'
 import Credits from './pages/Credits'
 import Community from './pages/Community'
+import { LuMenu } from 'react-icons/lu'
 
 const App = () => { // gradient
+    const [isMenuOpen, setIsMenuOpen] = useState(null);
     return (
         <> 
+        {!isMenuOpen && <LuMenu className='absolute top-3 left-3 w-8 h-8 cursor-pointer 
+        md:hidden not-dark:invert text-fuchsia-50' onClick={()=>setIsMenuOpen(true)}/>}
         <div className='dark:bg-linear-to-b from-[#242124] to-[#000000]
         dark:text-white'>  
         <div className='flex h-screen w-screen'>
-                <Sidebar />
+                <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
                 <Routes>
                     <Route path='/' element={<ChatBox />} />
                     <Route path='/credits' element={<Credits />} />
