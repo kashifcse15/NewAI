72fe1360aa9a2d1228b0606e1b60e025cfd01dee
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-06-10 17:31:36 +0530

    Updated Credits Page

--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { dummyPlans } from '../assets/assets/assets'
 
 const Credits = () => {
+  const [plans, setPlans] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchPlans = async () => {
+    setPlans(dummyPlans);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchPlans();
+  }, []);
+
+
   return (
-    <div>Credits</div>
+    <div className='max-w-7xl h-screen overflow-y-scroll mx-auto px-4 sm:px-6 lg:px-8 py-12'>
+      <h1 className='text-3xl font-bold text-center mb-10 xl:mt-30 text-gray-900 dark:text-white'>Credit Plans</h1>
+
+      <div className='flex flex-wrap justify-center gap-8'>
+        {plans.map((plan) => (
+          <div key={plan._id} className={`border border-gray-300 dark:border-green-800 rounded-lg shadow hover:shadow-lg transition-shadow p-6 min-w-[300px] 
+            flex flex-col ${plan._id === "pro" ? "bg-green-100 dark:bg-green-800" : "bg-white dark:bg-transparent"
+            }`} >
+            <div className='flex-1'>
+              <h3 className='text-xl font-semibold text-gray-900 dark:text-white mb-2'>{plan.name}</h3>
+              <p className='text-2xl font-bold text-green-600 dark:text-green-300 mb-4'>&{plan.price}
+                <span className='text-base font-normal text-gray-600 dark:text-green-300'>{plan.credits} / credits</span>
+              </p>
+              <ul className='list-disc list-inside text-sm text-gray-700 dark:text-green-300 space-y-1'>
+                {plan.features.map((feature, index) => (
+                  <li key={index}>{feature}</li>
+                ))}
+              </ul>
+            </div>
+            <button className='mt-6 bg-green-600 hover:bg-green-700 active:bg-green-800 text-white font-medium 
+            py-2 rounded transition-colors cursor-pointer'>Buy Now</button>
+
+          </div>
+        ))}
+
+      </div>
+    </div>
   )
 }
 
